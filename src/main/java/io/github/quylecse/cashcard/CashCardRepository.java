87842999b8bca385b023d5914e8c99ae17505f2b
quylecse <mr.lpquy@gmail.com>
2025-05-20 22:26:53 +0200

    edit CCR

--- src/main/java/io/github/quylecse/cashcard/CashCardRepository.java
+++ src/main/java/io/github/quylecse/cashcard/CashCardRepository.java
@@ -3,4 +3,5 @@ package io.github.quylecse.cashcard;
 import org.springframework.data.repository.CrudRepository;
 
 interface CashCardRepository extends CrudRepository<CashCard, Long> {
+    // This interface is intentionally left blank. It inherits all CRUD operations from CrudRepository.
 }
